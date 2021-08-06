a5af924c257ebbd94ca7f8d2e5348307cb38da5f
Ayaan Lone <ayaan.lone12@gmail.com>
2021-08-06 23:15:15 +0530

    Deleted heroku

--- BOOKUET/settings.py
+++ BOOKUET/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'BOOKUET.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'da287m14ikgek1',
-        'USER': 'hyivgdtvrthqsq',
-        'PASSWORD': '3e87e5a1c569c56ac01dbf7a304efda7934da6e36d00b812f48c70465d9cb872',
-        'HOST': 'ec2-34-197-105-186.compute-1.amazonaws.com',
-        'PORT' : 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+# }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'da287m14ikgek1',
+#         'USER': 'hyivgdtvrthqsq',
+#         'PASSWORD': '3e87e5a1c569c56ac01dbf7a304efda7934da6e36d00b812f48c70465d9cb872',
+#         'HOST': 'ec2-34-197-105-186.compute-1.amazonaws.com',
+#         'PORT' : 5432,
         
        
-    }
+#     }
 }
 # DATABASES = {
 #     'default':dj_database_url.config()
